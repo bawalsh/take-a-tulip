4a026371100387418ecc62231ed3608de6b37aaa
Brent Walsh <mail@brentwalsh.net>
2017-11-09 11:56:00 +0200

    Initial commit - Add webpack config - Add scripts to build and deploy to firebase - Add simple index page

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <p>Take a Tulip</p>
+        )
+    }
+}
+
+module.exports = App;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+const App = require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
